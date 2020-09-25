9b5f929d28b22b7b3831f1b9315b53a62ba79505
phidangthanh <dtphi.khtn@gmail.com>
2020-09-25 08:45:52 +0700

    init json msg api

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,11 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -29,7 +33,7 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Throwable  $exception
+     * @param  \Throwable $exception
      * @return void
      *
      * @throws \Throwable
@@ -40,16 +44,60 @@ class Handler extends ExceptionHandler
     }
 
     /**
+     * @author change : Phi .
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Throwable  $exception
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Throwable $exception
      * @return \Symfony\Component\HttpFoundation\Response
      *
      * @throws \Throwable
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        $jsEx = new JsonMsgCommon(['msg' => 'Server error'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        if ($this->isQueryExp($exception)) {
+            $jsEx = new JsonMsgCommon(['msg' => 'No connection to db'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        } elseif ($this->isModelExp($exception)) {
+            $jsEx = new JsonMsgCommon(['msg' => 'Invalid Parameter Value in the Request.'], Response::HTTP_NOT_FOUND);
+        } elseif ($this->isHttpExp($exception)) {
+            $jsEx = new JsonMsgCommon(['msg' => 'Invalid Route or Parameter in the Request.'], Response::HTTP_BAD_REQUEST);
+        }
+
+        return $request->wantsJson() ? $jsEx : parent::render($request, $exception);
+    }
+
+    /**
+     * @author : Phi .
+     * @param $exception
+     * @return bool
+     */
+    public function isQueryExp($exception)
+    {
+        return $exception instanceof QueryException;
+    }
+
+    /**
+     * @author : Phi .
+     * if its a model not found exception
+     * @param $exception
+     *
+     * @return bool
+     */
+    public function isModelExp($exception)
+    {
+        return $exception instanceof ModelNotFoundException;
+    }
+
+    /**
+     * @author : Phi .
+     * if its an http not found exception
+     * @param $exception
+     *
+     * @return bool
+     */
+    public function isHttpExp($exception)
+    {
+        return $exception instanceof NotFoundHttpException;
     }
 }
